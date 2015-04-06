7a3f5798753318e96f7cc37f4fd226f78a5455b7
Ari Fajrianda Alfi <ryan.alfi@gmail.com>
2015-04-06 23:50:48 +0700

    TP8

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        final String nama="Ari Fajrianda Alfi";
+        final String nim="6306134091";
+        
+        System.out.println("Hello, Saya "+nama+". Nim Saya "+nim+". saya sangat menyukai praktikum RPL Modul 8 ini");
+    }
+    
+}
